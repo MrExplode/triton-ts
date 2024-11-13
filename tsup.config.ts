aab2f88a0b516945fa695bf717a2e1a614f27201
MrExplode <me@sunstorm.rocks>
2024-11-13 01:48:22 +0100

    feat: disable minify, enable sourcemaps

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
     entry: ['src/main.ts'],
     clean: true,
-    minify: true,
     dts: true,
+    sourcemap: true,
     format: ['esm', 'cjs']
 })
